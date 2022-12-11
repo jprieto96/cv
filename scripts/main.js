5c7f22b4775ffeba8fe692afa627d172b0b8fe54
Jose Prieto <joseprieto9657@gmail.com>
2022-12-11 19:00:51 +0100

    Added functionality to send email

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,23 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+
+        const serviceID = 'default_service';
+        const templateID = 'template_6azf0y6';
+
+
+        // these IDs from the previous steps
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
